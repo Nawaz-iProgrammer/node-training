0b625ce0f498045205a660b9276f2a79dbbe429c
Nawaz-iProgrammer <mohamedn@iprogrammer.co>
2022-06-27 17:53:53 +0530

    Dynamic pages with templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const path = require('path');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('views', 'public/views');
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'John',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'Weather',
+    name: 'John',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    info: 'Help page is yet to ready',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
