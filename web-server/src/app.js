6028527053ea51654ddcd4df12b4464665e3864d
Nawaz-iProgrammer <mohamedn@iprogrammer.co>
2022-06-27 18:51:20 +0530

    Advanced templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.set('views', 'public/views');
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../public/templates/views');
+const partialsPath = path.join(__dirname, '../public/templates/partials');
+
+// Setup handlebar engine and views location
+app.set('views', viewsPath);
 app.set('view engine', 'hbs');
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
@@ -17,21 +26,23 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    title: 'Weather',
-    name: 'John',
+    title: 'About me',
+    name: 'Programmer',
   });
 });
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    info: 'Help page is yet to ready',
+    helpText: 'You can find help here',
+    title: 'Help',
+    name: 'John',
   });
 });
 
 app.get('/weather', (req, res) => {
   res.send({
-    state: 'Ke',
-    climate: 'Rain',
+    forecast: 'Rain',
+    Location: 'Delhi',
   });
 });
 
