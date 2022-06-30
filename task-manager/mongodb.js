f6dc11f23137aa800a89f76c972f66c9f6b9296e
Nawaz-iProgrammer <mohamedn@iprogrammer.co>
2022-06-30 14:36:24 +0530

    Querying documents

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -3,11 +3,6 @@ const { MongoClient, ObjectId } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectId();
-console.log(id.toString());
-console.log(id.getTimestamp().toLocaleTimeString());
-console.log(id.id);
-
 MongoClient.connect(connectionURL, (error, client) => {
   if (error) {
     console.log('Something went wrong');
@@ -15,43 +10,13 @@ MongoClient.connect(connectionURL, (error, client) => {
 
   const db = client.db(databaseName);
 
-  // db.collection('users').insertOne(
-  //   {
-  //     name: 'Peter',
-  //     age: 30,
-  //   },
-  //   (error, result) => {
-  //     if (error) {
-  //       console.log(error);
-  //     }
+  db.collection('users')
+    .find({ age: 30 })
+    .coun((error, result) => {
+      if (error) {
+        return console.log(error);
+      }
 
-  //     console.log(result);
-  //   }
-  // );
-
-  // db.collection('users').insertMany([
-  //   {
-  //     name: 'Kane',
-  //     age: 31,
-  //   },
-  //   {
-  //     name: 'Orton',
-  //     age: 32,
-  //   },
-  // ]);
-
-  // db.collection('tasks').insertMany([
-  //   {
-  //     description: 'Clean the mirror',
-  //     completed: true,
-  //   },
-  //   {
-  //     description: 'Water the plants',
-  //     completed: false,
-  //   },
-  //   {
-  //     description: 'Repair T.V',
-  //     completed: false,
-  //   },
-  // ]);
+      console.log(result);
+    });
 });
