d7e84362b512e14d2515bd30130a90e7af8ecd6f
Nawaz-iProgrammer <mohamedn@iprogrammer.co>
2022-06-30 19:47:54 +0530

    Data validation & sanitization part1

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,45 +1,65 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    validate(value) {
+      const schema = Joi.string().email().validate(value);
+      if (schema.error) {
+        throw new Error('Enter a valid email');
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be of positive number');
+      }
+    },
   },
 });
 
-// const me = new User({
-//   name: 'Rock',
-//   age: 56,
-// });
-
-// me.save()
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-  },
-  completed: {
-    type: Boolean,
-  },
+const me = new User({
+  name: '   Edge   ',
+  email: '   edge@gmail.com  ',
 });
 
-const tasks = new Task({ description: 'Buy grocery', completed: false });
-
-tasks
-  .save()
+me.save()
   .then((result) => {
     console.log(result);
   })
   .catch((error) => {
     console.log(error);
   });
+
+// const Task = mongoose.model('Task', {
+//   description: {
+//     type: String,
+//   },
+//   completed: {
+//     type: Boolean,
+//   },
+// });
+
+// const tasks = new Task({ description: 'Buy grocery', completed: false });
+
+// tasks
+//   .save()
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
