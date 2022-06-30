6feaf2f35b2495f4cb896687d8e1fbf45b68922d
Nawaz-iProgrammer <mohamedn@iprogrammer.co>
2022-06-30 17:30:17 +0530

    Setting up mongoose

---
+++ task-manager/src/db/mongoose.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
+
+const me = new User({
+  name: 'Rock',
+  age: 56,
+});
+
+me.save()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
