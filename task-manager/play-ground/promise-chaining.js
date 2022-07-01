645ae60776726003eaaac710eec8b63fee253945
Nawaz-iProgrammer <mohamedn@iprogrammer.co>
2022-07-01 17:58:30 +0530

    Async/Await part 2

--- task-manager/play-ground/promise-chaining.js
+++ task-manager/play-ground/promise-chaining.js
@@ -2,11 +2,25 @@ require('../src/db/mongoose');
 
 const User = require('../src/model/user');
 
-User.findByIdAndUpdate('62be83a1efc223e766bc7e0a', { age: 1 })
-  .then((result) => {
-    console.log(result);
-    return User.countDocuments({ age: 1 });
-  })
+// User.findByIdAndUpdate('62be83a1efc223e766bc7e0a', { age: 1 })
+//   .then((result) => {
+//     console.log(result);
+//     return User.countDocuments({ age: 1 });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount('62be83a1efc223e766bc7e0a', 8)
   .then((result) => {
     console.log(result);
   })
--- task-manager/play-ground/promise-chaining2.js
+++ task-manager/play-ground/promise-chaining2.js
@@ -2,11 +2,25 @@ require('../src/db/mongoose');
 
 const Task = require('../src/model/task');
 
-Task.findByIdAndDelete('62beb3bb1845d478a96401d7')
-  .then((result) => {
-    console.log(result);
-    return Task.find({ completed: false });
-  })
+// Task.findByIdAndDelete('62beb3bb1845d478a96401d7')
+//   .then((result) => {
+//     console.log(result);
+//     return Task.find({ completed: false });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+const task = async (id, status) => {
+  const deleteTask = await Task.findByIdAndDelete(id);
+  const uncompleted = await Task.countDocuments({ completed: status });
+  return uncompleted;
+};
+
+task('62be8677fd2882b1a4f9b775', false)
   .then((result) => {
     console.log(result);
   })
